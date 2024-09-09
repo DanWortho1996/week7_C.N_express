e1dabf0fe6519310502fccc51da71fca742439c4
Daniel Worthington <daniel.worthington96@outlook.com>
2024-09-09 13:36:11 +0100

    finished static server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
+//https://localhost:5000/beetlejuice
+//https://mywebsite/beetlejuice
+
+app.use("/beetlejuice", express.static("beetlejuice"));
+
 app.listen(5000, () => {
     console.log("Server is listening on port 5000");
 });
-
