d07b44d7e1c4f184c1b31cfe2da9108691e414dc
Daniel Worthington <daniel.worthington96@outlook.com>
2024-09-10 11:59:42 +0100

    clean up code ready for mongoose

--- src/server.js
+++ src/server.js
@@ -4,28 +4,21 @@ const app = express();
 
 app.use(express.json());
 
-const fakeDB = [];
+// const fakeDB = [];
 
 //Get all books
 app.get("/books/allbooks", (request, response) => {
-    response.send({message: "success", books: fakeDB});
+    response.send({message: "success"});
 });
 
 //This is a root "/" to pull/target the next page or section it in arrays
 app.get("/books", (request, response) => {
-    
-     const book = {
-        title: "book 1",
-        author: "suzie",
-        genre: "comedy",
-     }
-    response.send({message: "success", book: book});
+    response.send({message: "success"});
 });
 
 //Post route
 app.post("/books", (request, response) => {
-    fakeDB.push(request.body);
-    response.send({message: `${request.body.title} has been added.`})
+    response.send({message: "success"})
 });
 
 app.listen(5000, () => {
